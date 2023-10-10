c4f0277bfc400db778b9a5a716bfb346eed9c732
Ash Woods <ash@whatup.email>
2023-10-09 19:26:29 -0700

    error handling

--- app/handlers.js
+++ app/handlers.js
@@ -3,50 +3,46 @@ const { getFinalUrl } = require('./get-final-url');
 const { hashFunction } = require('./hash');
 const cheerio = require('cheerio');
 
+const throwFormattedError = (errorMessage) => {
+    const formattedError = new Error(`${errorMessage}: ${error.message}`);
+    formattedError.stack = error.stack;
+    throw formattedError;
+}
+
 const handlers = module.exports.handlers = [
     {
         criteria: (resource) => resource.type === 'url' && !resource.meta?.resolved,
         handle: async (resource) => {
-            try {
-                const finalUrl = await getFinalUrl(resource.data);
+            const finalUrl = await getFinalUrl(resource.data)
+                .catch(throwFormattedError(`Failed to fetch URL: ${resource.meta.url}`));
 
-                // Create and return a new resource
-                return [{
-                    type: 'url',
-                    meta: {
-                        resolved: true,
-                    },
-                    data: finalUrl
-                }];
-            } catch (error) {
-                // Handle the error, maybe log it or return an error resource
-                error.message = "Failed to fetch URL:" + resource.meta.url;
-                throw error;
-            }
+            // Create and return a new resource
+            return [{
+                type: 'url',
+                meta: {
+                    resolved: true,
+                },
+                data: finalUrl
+            }];
         }
     },
     {
         criteria: (resource) => resource.type === 'url' && resource.meta?.resolved,
         handle: async (resource) => {
-            try {
-                const response = await axios.get(resource.data);
+            const response = await axios.get(resource.data)
+                .catch(throwFormattedError(`Failed to fetch URL: ${resource.meta.url}`));
 
-                // Create and return a new resource
-                return [{
-                    type: 'html',
-                    meta: {
-                        url: resource.data,
-                        status: response.status,
-                        contentType: response.headers['content-type'],
-                        // ... any other necessary fields
-                    },
-                    data: response.data
-                }];
-            } catch (error) {
-                // Handle the error, maybe log it or return an error resource
-                error.message = "Failed to fetch URL:" + resource.meta.url;
-                throw error;
-            }
+            // Create and return a new resource
+            return [{
+                type: 'html',
+                meta: {
+                    url: resource.data,
+                    status: response.status,
+                    contentType: response.headers['content-type'],
+                    // ... any other necessary fields
+                },
+                data: response.data
+            }];
         }
     },
     {
