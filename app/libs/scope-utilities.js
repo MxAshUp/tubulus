ca4799e8b672f607b24274275f2319aea9f8405a
Ash Woods <ash@whatup.email>
2023-10-24 20:49:44 -0700

    rename var

--- app/libs/scope-utilities.js
+++ app/libs/scope-utilities.js
@@ -2,8 +2,8 @@
 const typeEquals             = (type) => (resource) => resource.type === type;
 
 // Combinators (predicate dispatch)
-const some                   = (...predicates) => (val) => predicates.some((fn) => fn(val));
-const every                  = (...predicates) => (val) => predicates.every((fn) => fn(val));
+const some                   = (...predicates) => (resource) => predicates.some((fn) => fn(resource));
+const every                  = (...predicates) => (resource) => predicates.every((fn) => fn(resource));
 
 // "Binds" heach handler to all predicates
 const bindScope = (...predicate) => (handlers) => handlers.map(({scope, ...handlerArgs}) => ({
