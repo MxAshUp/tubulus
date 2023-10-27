365518ff8d85a1c6d6cf71ad15dfbe31e479e1e1
Ash Woods <ash@whatup.email>
2023-10-26 22:11:54 -0700

    Add subject for unhandled resources

--- app/libs/resource-crawler.js
+++ app/libs/resource-crawler.js
@@ -1,4 +1,4 @@
-const { from, combineLatest, of, fromEvent, pipe } = require('rxjs');
+const { from, combineLatest, of, fromEvent, pipe, Subject, EMPTY } = require('rxjs');
 const { filter, mergeMap, expand, map, mergeAll } = require('rxjs/operators');
 const { handledResultsToObservable } = require('./utilities.js');
 
@@ -16,7 +16,6 @@ module.exports.resourceCrawler = (options = {}) => {
 
     // Some possible observables to use
     const topResources$ =               fromResourceFind({ depth: 0 });
-    const unhandledResources$ =         fromResourceFind({ handled: false, orphaned: false });
     const unhandledTopLevelResources$ = fromResourceFind({ handled: false, orphaned: false, depth: 0 });
     const orphanedResources$ =          fromResourceFind({ handled: false, orphaned: true });
 
@@ -28,10 +27,13 @@ module.exports.resourceCrawler = (options = {}) => {
         filter(doc => doc.isFromCache() && !doc.handled && !doc.orphaned),
     );
 
+    // Resources emitted with no handler
+    const unhandledResources$ = new Subject();
 
     // The main handler of a resource, outputs 0 to many resources
     const resourceHandleTick = pipe(
-
+        // Nothing to do with the special EMPTY types
+        filter(({type}) => type !== "EMPTY"),
         // Takes a resource, and outputs an array of that resource paired with each matched handler
         // Also marks resource as handled
         // If no matching handlers
@@ -40,6 +42,11 @@ module.exports.resourceCrawler = (options = {}) => {
             
             // @TODO - should be await
             resource.setHandled(handlers);
+
+            if(!handlers.length) {
+                unhandledResources$.next(resource);
+                return EMPTY;
+            }
 
             return combineLatest([
                 of(resource),
